35b89418ea50338c22f637d14943de47286b95d0
Drew Love <davidandrewlove84@gmail.com>
2020-04-14 13:27:57 -0400

    hard code API_TOKEN for Authorizaton

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export default {
     API_ENDPOINT_LOCAL: 'http://localhost:9000/api',
     API_ENDPOINT: 'https://stormy-shore-17390.herokuapp.com/',
     HEADERS: {
-        "Authorization": process.env.REACT_APP_API_TOKEN, 
+        "Authorization": 'Bearer 1234', 
         "Content-Type": "application/json"
     },
 }
