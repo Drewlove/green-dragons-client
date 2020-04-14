e86818a0c93b4b1ba27311e201f82ef90cdaad14
Drew Love <davidandrewlove84@gmail.com>
2020-04-14 13:07:14 -0400

    add API_ENDPOINT_LOCAL and API_ENDPOINT

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-    API_ENDPOINT: 'http://localhost:9000/api',
+    API_ENDPOINT_LOCAL: 'http://localhost:9000/api',
+    API_ENDPOINT: 'https://stormy-shore-17390.herokuapp.com/',
     HEADERS: {
         "Authorization": process.env.REACT_APP_API_TOKEN, 
         "Content-Type": "application/json"
