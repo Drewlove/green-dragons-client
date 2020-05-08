aa6d6a7a919c4e841306bc032df1fdc6e63e6b66
Drew Love <davidandrewlove84@gmail.com>
2020-05-08 11:26:07 -0400

    tests now work

--- src/Students/StudentExchanges/StudentExchangeForm.test.js
+++ src/Students/StudentExchanges/StudentExchangeForm.test.js
@@ -2,11 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import StudentExchangeForm from './StudentExchangeForm'
 
+const exchange = 
+{
+    exchange_id: 1,
+    exchange_date: new Date('11-11-1980'),
+    student_id: 1,
+    amount: "1.00",
+    note: 'Note is'
+}
+
+const students = [
+    {first_name: 'Jon', last_name: 'Doe', student_id: 1},
+    {first_name: 'Jane', last_name: 'Doe', student_id: 2}
+]
+
+const invalidInputs = []
+
 describe('StudentExchangeForm', () => {
     it('renders without crashing', () => {
         const div = document.createElement('div')
         ReactDOM.render(
-            <StudentExchangeForm />, 
+            <StudentExchangeForm exchange={exchange} students={students} invalidInputs={invalidInputs}/>, 
             div
         )
         ReactDOM.unmountComponentAtNode(div)
--- src/Students/StudentExchanges/StudentExchangeFormContainer.test.js
+++ src/Students/StudentExchanges/StudentExchangeFormContainer.test.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {BrowserRouter} from 'react-router-dom'
 import StudentExchangeFormContainer from './StudentExchangeFormContainer'
 
 describe('StudentExchangeFormContainer', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(
-        <StudentExchangeFormContainer/>, 
-        div
-    )
-    ReactDOM.unmountComponentAtNode(div)
+    it('renders without crashing', () => {
+        const div = document.createElement('div')
+        ReactDOM.render(
+            <BrowserRouter>
+                <StudentExchangeFormContainer/>
+            </BrowserRouter>, 
+            div
+        )
+        ReactDOM.unmountComponentAtNode(div)
+    })
 })
