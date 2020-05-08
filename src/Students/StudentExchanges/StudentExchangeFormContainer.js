710906ff032d5945c8eedff3f8f8ff8bac11be6d
Drew Love <davidandrewlove84@gmail.com>
2020-05-08 11:37:18 -0400

    testing

--- src/Students/StudentExchanges/StudentExchangeFormContainer.js
+++ src/Students/StudentExchanges/StudentExchangeFormContainer.js
@@ -39,7 +39,7 @@ class StudentExchangeFormContainer extends Component{
     async getStudentsAndExchange(){
         const students = await this.getAllRowsFromEndpoint('students')
         const exchangeRaw = await this.getRowFromEndpoint('exchanges')
-        console.log(students, exchangeRow)
+        console.log(students, exchangeRaw)
         const exchange = this.reformatDate(exchangeRaw)
         this.setState({students})
         this.setState({exchange}, () => this.setState({isLoaded: true}))
