b46144f68e80dfce9e8b305fd8b2b5a44b4f8429
Drew Love <davidandrewlove84@gmail.com>
2020-05-09 10:32:46 -0400

    reformat StudentsListItem with grid layout

--- src/Students/StudentsListItem.js
+++ src/Students/StudentsListItem.js
@@ -3,7 +3,6 @@ import studentIcon from '../Assets/student.png'
 import ChallengesIcon from '../Assets/challenges.png'
 import CommunitiesIcon from '../Assets/communities.png'
 import {Link} from 'react-router-dom'
-
 const StudentsListItem = (props) => {
     const {first_name, last_name, student_id} = props.student
     return(
@@ -11,26 +10,53 @@ const StudentsListItem = (props) => {
         <div className='students-list-item-name'>
             <p>{first_name} {last_name}</p>
         </div>
-        <div className='students-list-item-links-wrapper'>
-            <Link className='students-list-item-link' to={`/students/${student_id}/profile`}>
-                <img className='students-list-item-link-icon' alt='student icon' src={studentIcon}/>
-                <p className='students-list-item-link-tooltip'>Profile</p>
-            </Link>
-            <Link className='students-list-item-link' to={`/students/${student_id}/communities`}>
-                <img className='students-list-item-link-icon' alt='communities icon' src={CommunitiesIcon}/>
-                <p className='students-list-item-link-tooltip'>Communities</p>                     
-            </Link>
-            <Link className='students-list-item-link' to={`/students/${student_id}/challenges`}>
-                <img className='students-list-item-link-icon' alt='challenges icon' src={ChallengesIcon}/>
-                <p className='students-list-item-link-tooltip'>Challenges</p>
-            </Link>
-            <Link className='students-list-item-link' to={`/students/${student_id}/exchanges`}>
-                <div className='students-list-item-link-exchanges'>$100.00</div>
-                <p className='students-list-item-link-tooltip'>Transactions</p>
-            </Link>
-        </div>
+        <Link className='students-list-item-link' to={`/students/${student_id}/profile`}>
+            <img className='students-list-item-link-icon' alt='student icon' src={studentIcon}/>
+            <p className='students-list-item-link-tooltip'>Profile</p>
+        </Link>
+        <Link className='students-list-item-link' to={`/students/${student_id}/communities`}>
+            <img className='students-list-item-link-icon' alt='communities icon' src={CommunitiesIcon}/>
+            <p className='students-list-item-link-tooltip'>Communities</p>                     
+        </Link>
+        <Link className='students-list-item-link' to={`/students/${student_id}/challenges`}>
+            <img className='students-list-item-link-icon' alt='challenges icon' src={ChallengesIcon}/>
+            <p className='students-list-item-link-tooltip'>Challenges</p>
+        </Link>
+        <Link className='students-list-item-link' to={`/students/${student_id}/exchanges`}>
+            <div className='students-list-item-link-exchanges'>$100.00</div>
+            <p className='students-list-item-link-tooltip'>Transactions</p>
+        </Link>
     </li>
     )
 }
+
+// const StudentsListItem = (props) => {
+//     const {first_name, last_name, student_id} = props.student
+//     return(
+//         <li className='students-list-main-item'>
+//         <div className='students-list-item-name'>
+//             <p>{first_name} {last_name}</p>
+//         </div>
+//         <div className='students-list-item-links-wrapper'>
+//             <Link className='students-list-item-link' to={`/students/${student_id}/profile`}>
+//                 <img className='students-list-item-link-icon' alt='student icon' src={studentIcon}/>
+//                 <p className='students-list-item-link-tooltip'>Profile</p>
+//             </Link>
+//             <Link className='students-list-item-link' to={`/students/${student_id}/communities`}>
+//                 <img className='students-list-item-link-icon' alt='communities icon' src={CommunitiesIcon}/>
+//                 <p className='students-list-item-link-tooltip'>Communities</p>                     
+//             </Link>
+//             <Link className='students-list-item-link' to={`/students/${student_id}/challenges`}>
+//                 <img className='students-list-item-link-icon' alt='challenges icon' src={ChallengesIcon}/>
+//                 <p className='students-list-item-link-tooltip'>Challenges</p>
+//             </Link>
+//             <Link className='students-list-item-link' to={`/students/${student_id}/exchanges`}>
+//                 <div className='students-list-item-link-exchanges'>$100.00</div>
+//                 <p className='students-list-item-link-tooltip'>Transactions</p>
+//             </Link>
+//         </div>
+//     </li>
+//     )
+// }
 
 export default StudentsListItem
