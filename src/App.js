18543e8d7057c75bab217f5d53140cb0b7da9297
Drew Love <davidandrewlove84@gmail.com>
2020-05-19 13:11:21 -0400

    display home page by default

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component{
     return (
       <>
         <Nav />
-        <Route path='/home' component={HomePage} />
+        <Route path='/' component={HomePage} />
         <Route path='/challenges' component={ChallengesRouting} />
         <Route path='/communities' component={CommunitiesRouting} />
         <Route path='/students' component={StudentsRouting} />
