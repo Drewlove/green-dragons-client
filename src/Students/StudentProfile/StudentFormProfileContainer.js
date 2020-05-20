96ae879370acaa12a89fbda0329c470b912a347b
Drew Love <davidandrewlove84@gmail.com>
2020-05-20 13:53:41 -0400

    scroll to top on save

--- src/Students/StudentProfile/StudentFormProfileContainer.js
+++ src/Students/StudentProfile/StudentFormProfileContainer.js
@@ -47,6 +47,7 @@ class StudentFormProfileContainer extends Component{
     renderModal(){
         HIDE_FORM()
         document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
         return(
             <Modal closeModal={()=> this.closeModal()}>
                 <p>{this.state.modalMessage}</p>
