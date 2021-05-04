48a1e16055a1f14e5fd1db7b06356ab4372ec8b8
Drew Love <davidandrewlove84@gmail.com>
2021-05-04 16:32:46 -0400

    Remove console.logs to find out why production environment app was encountering a server error

--- src/Utilities/HttpMethods.js
+++ src/Utilities/HttpMethods.js
@@ -1,63 +1,66 @@
-import config from '../config'
+import config from "../config";
 
 export const HTTP_METHODS = {
-    getData: endpointSuffix => getData(endpointSuffix),
-    submitData: (data, endpointSuffix, method) => submitData(data, endpointSuffix, method), 
-    deleteData: urlSuffix => deleteData(urlSuffix)   
-}
+  getData: (endpointSuffix) => getData(endpointSuffix),
+  submitData: (data, endpointSuffix, method) =>
+    submitData(data, endpointSuffix, method),
+  deleteData: (urlSuffix) => deleteData(urlSuffix),
+};
 
-const getData = async function(endpointSuffix){
-    const url = `${config.API_ENDPOINT}/${endpointSuffix}`
-    const options = {
-        method: "GET", 
-        headers: config.HEADERS
-    }
+const getData = async function (endpointSuffix) {
+  const url = `${config.API_ENDPOINT}/${endpointSuffix}`;
+  const options = {
+    method: "GET",
+    headers: config.HEADERS,
+  };
 
-    let result = {
-        ok: false, 
-        error: '',
-        data: {}
-    }
+  let result = {
+    ok: false,
+    error: "",
+    data: {},
+  };
 
-    try{
-        const response = await fetch(url, options)
-        return response.ok ? result = {...result, ok: true, data: await response.json()} : result = {...result, ok: false, error: `${response.statusText}`}
-    } catch(error){
-        return result = {ok: false, error: 'Error. Could not retrieve record'}   
-    }
-}
+  try {
+    const response = await fetch(url, options);
+    return response.ok
+      ? (result = { ...result, ok: true, data: await response.json() })
+      : (result = { ...result, ok: false, error: `${response.statusText}` });
+  } catch (error) {
+    return (result = { ok: false, error: "Error. Could not retrieve record" });
+  }
+};
 
-const submitData = async function(data, endpointSuffix, method){
-    const options = {
-        method: method,
-        body: JSON.stringify(data), 
-        headers: {
-            "Authorization" : config.API_KEY, 
-            "Content-Type": "application/json", 
-        }
-    }
-    const url = `${config.API_ENDPOINT}/${endpointSuffix}`
-    try{
-        const result = await fetch(url, options)
-        return result 
-    } catch(error){
-        return error 
-    }
-}
+const submitData = async function (data, endpointSuffix, method) {
+  const options = {
+    method: method,
+    body: JSON.stringify(data),
+    headers: {
+      Authorization: config.API_KEY,
+      "Content-Type": "application/json",
+    },
+  };
+  const url = `${config.API_ENDPOINT}/${endpointSuffix}`;
+  try {
+    const result = await fetch(url, options);
+    return result;
+  } catch (error) {
+    return error;
+  }
+};
 
-const deleteData = async function(endpointSuffix){
-    const options = {
-        method: "DELETE", 
-        headers: {
-            "authorization": config.API_KEY, 
-            "content-type": "application/json"
-        }
-    }
-    const url = `${config.API_ENDPOINT}/${endpointSuffix}`
-    try {
-        const result = await fetch(url, options)
-        return result 
-    } catch(error){
-        return error 
-    }
-}
+const deleteData = async function (endpointSuffix) {
+  const options = {
+    method: "DELETE",
+    headers: {
+      authorization: config.API_KEY,
+      "content-type": "application/json",
+    },
+  };
+  const url = `${config.API_ENDPOINT}/${endpointSuffix}`;
+  try {
+    const result = await fetch(url, options);
+    return result;
+  } catch (error) {
+    return error;
+  }
+};
--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export default {
-    API_ENDPOINT: 'https://warm-ocean-22527.herokuapp.com/api',
-    // API_ENDPOINT: 'http://localhost:9000/api',
-    API_KEY: process.env.REACT_APP_API_KEY, 
-    HEADERS: {
-            "Authorization": process.env.REACT_APP_API_KEY, 
-            "Content-Type": "application/json"
-    }
-}
+  // API_ENDPOINT: 'https://warm-ocean-22527.herokuapp.com/api',
+  API_ENDPOINT: "http://localhost:9000/api",
+  API_KEY: process.env.REACT_APP_API_KEY,
+  HEADERS: {
+    Authorization: process.env.REACT_APP_API_KEY,
+    "Content-Type": "application/json",
+  },
+};
